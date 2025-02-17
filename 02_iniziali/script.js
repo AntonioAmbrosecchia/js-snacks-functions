4993c1cf80c9f83cdae4c496e73cb5ae567e5191
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-02-17 16:21:32 +0100

    end es.2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,14 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
-
-const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+function prendiIniziali(namesArr) {
+    return namesArr.map(name => name.charAt(0).toUpperCase());
+}
 
 
 // Dichiara la funzione qui.
-
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(prendiIniziali(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
