3b3ac1a0c52bde62cbe6fda7117735ffee8184bc
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-02-17 16:29:48 +0100

    end es.3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,13 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
-
-const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+function letterFilter(namesArr, letter) {
+    return namesArr.filter(name => name.charAt(0).toUpperCase() === letter.toUpperCase());
+}
 
 // Dichiara la funzione qui.
-
+const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(letterFilter(names, "A"));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
